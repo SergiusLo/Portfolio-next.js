ea37c07d5ffa060ae32424b7aa150e7f55fc92d4
Serhii <slobojko21@gmail.com>
2023-09-22 15:32:45 +0300

    about me section

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,97 @@
+"use client";
+import Image from "next/image";
+import { useState, useTransition } from "react";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>Tailwind</li>
+        <li>Typescript</li>
+        <li>JavaScript</li>
+        <li>Bootstrap</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>State Biotechnology University</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certification ",
+    id: "certification ",
+    content: <ul className="list-disc pl-2"></ul>,
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:py-16">
+        <Image
+          src="/images/about-image.png"
+          width={500}
+          height={500}
+          alt="about image"
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            A passionate frontend developer dedicated to building user-friendly
+            and high-performance web applications. With a strong knowledge of
+            React.js, TypeScript and Next.js, I excel in designing interactive
+            user interfaces, responsive web applications and website templates,
+            utilizing the power of Tailwind CSS and Bootstrap to achieve
+            exceptional results.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certification ")}
+              active={tab === "certification "}
+            >
+              {" "}
+              Certification
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
